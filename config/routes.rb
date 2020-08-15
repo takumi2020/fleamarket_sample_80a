9766d76a9a6da21a2e58b8cdc0aeebbd9da68906
joker99999 <jun.y8724@gmail.com>
2020-08-15 15:51:36 +0900

    sample

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
       get "signup_done"
     end
   end
+
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
