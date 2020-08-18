571bd56e7c1750e77b3c4df2743d28c9fb16ba1a
yiwatani <dbhammer17@gmail.com>
2020-08-18 17:16:28 +0900

    asynchronous communication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/items/#{comment.item.id}"
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to item_path(params[:item_id])  }
+      format.json
+    end
   end
 
   private
