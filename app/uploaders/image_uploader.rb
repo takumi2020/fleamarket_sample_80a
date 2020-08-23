c79aa14ec01620fdfe2adcd2066432b1ffced03b
sho-19881105 <sho198811052003@icloud.com>
2020-08-23 22:42:56 +0900

    added_extentions_of_image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
