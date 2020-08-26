06e380002c22cd8c7a0293523269755f4b03b4f3
takumi2020 <takumi_h@hamata9.com>
2020-08-26 16:36:44 +0900

    fix.edit.js

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,9 +1,4 @@
-$(function () {
-  $('#inputin').keyup(function(){
-      $('.count1000').text($(this).val().length);
-  });
-});
-$(function(){
+$(document).on('turbolinks:load', function() {
   //プレビューのhtmlを定義
   function buildHTML(count) {
     var html = `<div class="preview-box" id="preview-box__${count}">
@@ -128,8 +123,7 @@ $(function(){
   });
 });
 
-
-$(function () {
+$(document).on('turbolinks:load', function() { 
   $('[name=change]').click(function () {
     $('.change').remove();
     // location.reload();
@@ -217,90 +211,3 @@ $(function () {
           });
   });
 });
-
-// $(window).bind("load", function () {
-//   if (document.URL.match(/new/)) {
-//     function appendOption(category){
-//       var html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
-//       return html;
-//     }
-//     function appendChildrenBox(insertHTML){
-//       var childSelectHtml = '';
-//       childSelectHtml = `<div class='listing-select-wrapper__added' id= 'children_wrapper'>
-//                           <div class='listing-select-wrapper__box'>
-//                             <select class="inputfield" id="child_category" name="item[category_id]">
-//                               <option value="---" data-category="---">選択してください</option>
-//                               ${insertHTML}
-//                             <select>
-//                           </div>
-//                         </div>`;
-//       $('.listing-product-detail__category').append(childSelectHtml);
-//     }
-//     function appendGrandchildrenBox(insertHTML){
-//       var grandchildSelectHtml = '';
-//       grandchildSelectHtml = `<div class='listing-select-wrapper__added' id= 'grandchildren_wrapper'>
-//                                 <div class='listing-select-wrapper__box'>
-//                                   <select class="inputfield inputfield-last" id="grandchild_category" name="item[category_id]">
-//                                     <option value="---" data-category="---">選択してください</option>
-//                                     ${insertHTML}
-//                                   </select>
-//                                 </div>
-//                               </div>`;
-//       $('.listing-product-detail__category').append(grandchildSelectHtml);
-//     }
-//     $("#parent_category").on('change', function(){
-//       var parentCategory = document.getElementById('parent_category').value;
-//       if (parentCategory != "選択してください"){
-//         $.ajax({
-//           url: '/items/get_category_children',
-//           type: 'GET',
-//           data: { parent_name: parentCategory },
-//           dataType: 'json'
-//         })
-//         .done(function(children){
-//           $('#children_wrapper').remove();
-//           $('#grandchildren_wrapper').remove();
-//           var insertHTML = '';
-//           children.forEach(function(child){
-//             insertHTML += appendOption(child);
-//           });
-//           appendChildrenBox(insertHTML);
-//         })
-//         .fail(function(){
-//           alert('カテゴリー取得に失敗しました');
-//         })
-//       }else{
-//         $('#children_wrapper').remove();
-//         $('#grandchildren_wrapper').remove();
-//       }
-//     });
-//     $('.listing-product-detail__category').on('change', '#child_category', function(){
-//       var childId = $('#child_category option:selected').data('category');
-//       if (childId != "選択してください"){
-//         $.ajax({
-//           url: '/items/get_category_grandchildren',
-//           type: 'GET',
-//           data: { child_id: childId },
-//           dataType: 'json'
-//         })
-//         .done(function(grandchildren){
-//           if(grandchildren.length != 0) {
-//             $('#grandchildren_wrapper').remove();
-//             $('#size_wrapper').remove();
-//             $('#brand_wrapper').remove();
-//             var insertHTML = '';
-//             grandchildren.forEach(function(grandchild){
-//               insertHTML += appendOption(grandchild);
-//             });
-//             appendGrandchildrenBox(insertHTML);
-//           }
-//         })
-//         .fail(function(){
-//           alert('カテゴリー取得に失敗しました');
-//         })
-//       }else{
-//         $('#grandchildren_wrapper').remove();
-//       }
-//     });
-//   }
-// });
